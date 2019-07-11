45c6ec196e59f7d1f6b85035e95c2b692490d5c2
Alexander Lahtyuk <lahtyuk@factorov.ru>
2019-07-12 02:17:07 +0300

    Fix service definition

--- src/DependencyInjection/ExpressionCalculatorExtension.php
+++ src/DependencyInjection/ExpressionCalculatorExtension.php
@@ -3,14 +3,13 @@
 namespace horror\ExpressionCalculatorBundle\DependencyInjection;
 
 use horror\ExpressionCalculatorBundle\Adapters\AdapterInterface;
-use horror\ExpressionCalculatorBundle\Adapters\DefaultAdapter;
 use horror\ExpressionCalculatorBundle\ExpressionCalculator;
-use http\Exception\RuntimeException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Class ExpressionCalculatorExtension.
@@ -27,20 +26,21 @@ class ExpressionCalculatorExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+
         $calcDefinition = new Definition(ExpressionCalculator::class);
 
         $adapterClassName = $config['adapter_class'];
 
         $interfaces = class_implements($adapterClassName);
-        if(!is_array($interfaces) || !in_array(AdapterInterface::class, $interfaces, true)) {
+        if (!is_array($interfaces) || !in_array(AdapterInterface::class, $interfaces, true)) {
             throw new \RuntimeException(
                 "Adapter `$adapterClassName` in config `expression_calculator.adapter_class` must implement interface `horror\ExpressionCalculatorBundle\Adapters\AdapterInterface`"
             );
         }
 
-        $adapter = new $adapterClassName();
-
-        $calcDefinition->replaceArgument(0, $adapter);
+        $calcDefinition->addArgument(new Reference($adapterClassName));
         $calcDefinition->setPublic(true);
 
         $container->setDefinition(ExpressionCalculator::class, $calcDefinition);
